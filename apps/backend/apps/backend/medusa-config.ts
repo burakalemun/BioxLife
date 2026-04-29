8feb11b3e639f833462552d19b81a78f826ac4ba
burakalemun <61062491+burakalemun@users.noreply.github.com>
2026-04-29 19:40:35 +0300

    fix: add redisUrl and event bus to medusa config

--- apps/backend/apps/backend/medusa-config.ts
+++ apps/backend/apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     databaseDriverOptions: {
       connection: {
         ssl: {
@@ -22,6 +23,12 @@ module.exports = defineConfig({
     }
   },
   modules: [
+    {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
     {
       resolve: "./src/modules/fragrance",
     },
